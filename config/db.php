4c1f936de66e3d68c2d0c1353f6f206fda21777c
root <diwakarsharma603@gmail.com>
2020-08-23 21:42:08 +0530

    Update with joins functionality

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2joins',
+    'dsn' => 'mysql:host=localhost;dbname=yii_test',
     'username' => 'root',
-    'password' => 'root',
+    'password' => 'Root@123',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
